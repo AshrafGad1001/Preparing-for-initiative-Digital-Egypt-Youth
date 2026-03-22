9dd17ff7f168626ee9dbe12ffe739ee0caaf8324
AshrafGad1001 <ashrafgad542@gmail.com>
2026-03-22 14:27:03 +0200

    Day 05 Responsive webpage

---
+++ Day 04 node js/node js project/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/home', (req, res) => {
+    res.send('Landding page  !');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Hello from Ashraf !');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
